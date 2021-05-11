5f86ed1c10e6cd5cf0886f17bcc79372b9f12fdf
Hugo Aristizábal <huguetex@gmail.com>
2021-05-10 19:35:36 -0500

    Agregar StuffRepo y Singleton

---
+++ Cedesistemas.WheresMyStuff/Cedesistemas.WheresMyStuff.Repos/StuffRepo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Cedesistemas.WheresMyStuff.Models;
+
+namespace Cedesistemas.WheresMyStuff.Repos
+{
+    public sealed class StuffRepo
+    {
+        private static StuffRepo _instance;
+
+        public List<Stuff> StuffList { get; set; }
+
+        private StuffRepo()
+        {
+            StuffList = new List<Stuff>
+            {
+                new Stuff {
+                    Id = 1,
+                    Name = "Llaves del garaje",
+                    Location = "Segundo cajón del la biblioteca",
+                    DateTime = DateTime.Now,
+                    IsVisibleForAll = true
+                },
+                new Stuff {
+                    Id = 2,
+                    Name = "Control Remoto",
+                    Location = "Segundo cajón del la biblioteca",
+                    DateTime = DateTime.Now.AddDays(-1)
+                },
+                new Stuff {
+                    Id = 3,
+                    Name = "Caja de herramientas",
+                    Location = "Cuarto de linos"
+                },
+                new Stuff {
+                    Id = 4,
+                    Name = "Pilas recargables",
+                    Location = "Cuarto de linos",
+                    IsVisibleForAll = true
+                }
+            };
+        }
+
+        public static StuffRepo GetInstance()
+        {
+            //Opción 1:
+            //if (_instance == null)
+            //{
+            //    _instance = new StuffRepo();
+            //}
+            //return _instance;
+
+            //Opción 2:
+            //return _instance ?? (_instance = new StuffRepo());
+
+            return _instance ??= new StuffRepo();
+        }
+    }
+}
--- Cedesistemas.WheresMyStuff/Cedesistemas.WheresMyStuff.WebApi/Controllers/StuffController.cs
+++ Cedesistemas.WheresMyStuff/Cedesistemas.WheresMyStuff.WebApi/Controllers/StuffController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Cedesistemas.WheresMyStuff.Models;
+using Cedesistemas.WheresMyStuff.Repos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cedesistemas.WheresMyStuff.WebApi.Controllers
@@ -10,42 +11,28 @@ namespace Cedesistemas.WheresMyStuff.WebApi.Controllers
     [Route("[controller]/[action]/{id?}")]
     public class StuffController : ControllerBase
     {
-        private readonly List<Stuff> _stuffList; //Field - Class variable
+        private readonly StuffRepo _stuffRepo;
 
         public StuffController()
         {
-            _stuffList = new List<Stuff>
-            {
-                new Stuff {
-                    Id = 1,
-                    Name = "Llaves del garaje",
-                    Location = "Segundo cajón del la biblioteca",
-                    DateTime = DateTime.Now,
-                    IsVisibleForAll = true
-                },
-                new Stuff {
-                    Id = 2,
-                    Name = "Control Remoto",
-                    Location = "Segundo cajón del la biblioteca",
-                    DateTime = DateTime.Now.AddDays(-1)
-                },
-                new Stuff {
-                    Id = 3,
-                    Name = "Caja de herramientas",
-                    Location = "Cuarto de linos"
-                },
-                new Stuff {
-                    Id = 4,
-                    Name = "Pilas recargables",
-                    Location = "Cuarto de linos",
-                    IsVisibleForAll = true
-                }
-            };
+            _stuffRepo = StuffRepo.GetInstance();
         }
 
         public IActionResult GetAll()
         {
-            return Ok(_stuffList);
+            return Ok(_stuffRepo.StuffList);
+        }
+
+        public IActionResult Add()
+        {
+            _stuffRepo.StuffList.Add(new Stuff
+            {
+                Id = 5,
+                Name = "Reloj azul",
+                Location = "Mesa de noche",
+                DateTime = DateTime.Now.AddDays(-1)
+            });
+            return Ok(_stuffRepo.StuffList);
         }
 
         public IActionResult GetById(int id)
@@ -53,7 +40,7 @@ namespace Cedesistemas.WheresMyStuff.WebApi.Controllers
             //Linq
             //var stuff = _stuffList.FirstOrDefault(a => a.Id == id);
             //IEnumerable<Stuff> stuffList = _stuffList.Where(a => a.DateTime >= DateTime.Today.AddDays(-7) && a.Location.Contains("Linos"));
-            IEnumerable<Stuff> stuffList = _stuffList.Where(a => a.Id == id);
+            IEnumerable<Stuff> stuffList = _stuffRepo.StuffList.Where(a => a.Id == id);
             Stuff stuff = stuffList.FirstOrDefault();
 
             if (stuff == null)
